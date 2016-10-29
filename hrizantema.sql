36b7295de669a824e9ea2b6fca2bb76b2821d374
root <root@localhost.localdomain>
2016-10-29 22:01:35 +0300

    sql

--- hrizantema.sql
+++ hrizantema.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost
--- Время создания: Окт 14 2016 г., 08:39
--- Версия сервера: 5.7.15
--- Версия PHP: 5.6.26
+-- Время создания: Окт 29 2016 г., 18:59
+-- Версия сервера: 5.7.16
+-- Версия PHP: 5.6.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,6 +23,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `News`
+--
+
+CREATE TABLE `News` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `img` text,
+  `short_text` text,
+  `text` text NOT NULL,
+  `date` datetime NOT NULL,
+  `date_news` date NOT NULL,
+  `visible` tinyint(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `User`
 --
 
@@ -35,23 +53,32 @@ CREATE TABLE `User` (
   `verificationHash` text,
   `active` tinyint(1) NOT NULL,
   `authKey` varchar(255) DEFAULT NULL,
-  `accessToken` varchar(32) DEFAULT NULL
+  `accessToken` varchar(32) DEFAULT NULL,
+  `admin` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `User`
 --
 
-INSERT INTO `User` (`id`, `name`, `password`, `email`, `createTime`, `verificationHash`, `active`, `authKey`, `accessToken`) VALUES
-(1, 'fess_zorro', '439748b36d3da3ec077e29b69efde14b', 'vorobyev2.it@gmail.com', '2016-10-26 08:00:00', '', 1, '', ''),
-(2, 'qwerty', '439748b36d3da3ec077e29b69efde14b', 'voroby3v.it@gmail.com', '2016-10-10 11:06:03', 'o%2BMOYpeYRzw1uEwHkzGNRJZbUSshL3qQ9Chvdc1tzX4%3D', 0, NULL, NULL),
-(4, 'Михаил Воробьев', NULL, NULL, '2016-10-12 14:01:58', NULL, 1, 'google_oauth-113445715136324886195', NULL),
-(5, 'qwerty2', '439748b36d3da3ec077e29b69efde14b', 'vorobye3v.it@gmail.com', '2016-10-13 14:47:08', '4FTu67RKF0AjBmKOVxc1jnLkkjfLOsb2IV5c%2FNOjDx4%3D', 1, NULL, NULL),
-(6, 'qwerty3', '439748b36d3da3ec077e29b69efde14b', 'vorobyev.it@gmail.com', '2016-10-13 14:57:22', '0aBXEol9oDGDswij6GTC%2FC6LSbkly3HlfkH19YdxeGM%3D', 1, NULL, NULL);
+INSERT INTO `User` (`id`, `name`, `password`, `email`, `createTime`, `verificationHash`, `active`, `authKey`, `accessToken`, `admin`) VALUES
+(1, 'fess_zorro', '439748b36d3da3ec077e29b69efde14b', 'vorobyev2.it@gmail.com', '2016-10-26 08:00:00', '', 1, '', '', 1),
+(2, 'qwerty', '439748b36d3da3ec077e29b69efde14b', 'voroby3v.it@gmail.com', '2016-10-10 11:06:03', 'o%2BMOYpeYRzw1uEwHkzGNRJZbUSshL3qQ9Chvdc1tzX4%3D', 0, NULL, NULL, 0),
+(4, 'Михаил Воробьев', NULL, NULL, '2016-10-12 14:01:58', NULL, 1, 'google_oauth-113445715136324886195', NULL, 0),
+(5, 'qwerty2', '439748b36d3da3ec077e29b69efde14b', 'vorobye3v.it@gmail.com', '2016-10-13 14:47:08', '4FTu67RKF0AjBmKOVxc1jnLkkjfLOsb2IV5c%2FNOjDx4%3D', 1, NULL, NULL, 0),
+(6, 'qwerty3', '439748b36d3da3ec077e29b69efde14b', 'vorobyeeev.it@gmail.com', '2016-10-13 14:57:22', '0aBXEol9oDGDswij6GTC%2FC6LSbkly3HlfkH19YdxeGM%3D', 1, NULL, NULL, 0),
+(7, 'zorrozorrozorrozorro', '439748b36d3da3ec077e29b69efde14b', 'vorobyegv.it@gmail.com', '2016-10-21 11:27:27', 'b524oovzwlre5EdJx4FFeNVjPRUvOUEUZQMvVKEZke4%3D', 1, NULL, NULL, 0),
+(8, 'administrator', '21232f297a57a5a743894a0e4a801fc3', 'vorobyev.it@gmail.com', '2016-10-28 16:23:33', 'srABx10LTHfdMcJOAIEYmortPZSqxLC33Y1iD8dLLeM%3D', 1, NULL, NULL, 1);
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `News`
+--
+ALTER TABLE `News`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `User`
@@ -65,10 +92,15 @@ ALTER TABLE `User`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `News`
+--
+ALTER TABLE `News`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT для таблицы `User`
 --
 ALTER TABLE `User`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
